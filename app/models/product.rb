9a4c07dc074604d23af6a9ec624a08f81c8118ac
kainth01 <kainth247@gmail.com>
2017-11-08 15:03:55 -0600

    added support for SLIM preprocessor, and added Pages Controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :category
 
-    validates :name, :description, :price, presence: true
+    validates :name, presence: true, uniqueness: true
+    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root 'pages#home', as: 'home'
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
