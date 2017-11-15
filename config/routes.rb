2c39a05b4955c5f9245ff6f6bbae30fc8da99344
Harjot Kainth <harjot@kainth.ca>
2017-11-15 20:09:57 +0000

    adding seeds for activeadmin for production db, and also added some partials and started working on layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   root 'pages#home', as: 'home'
   get 'pages/home'
+  get 'pages/products', as: 'products'
+  get 'pages/about', as: 'about'
+  get 'pages/contac', as: 'contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
