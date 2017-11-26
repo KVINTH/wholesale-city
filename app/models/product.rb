9a8a8677a68f1c634743880a0f4c92872ee2ad43
kainth01 <kainth247@gmail.com>
2017-11-26 17:06:23 -0600

    worked on models for shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
     belongs_to :category
+    has_many :order_items
+
+    default_scope { where(active: true) }
 
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
