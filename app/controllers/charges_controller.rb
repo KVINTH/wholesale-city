d23512f6f0d3bc6e57bcd75e9f8b3ac16c52141c
kainth01 <kainth247@gmail.com>
2017-11-27 00:36:38 -0600

    no longer using hardcoded amount

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,13 +1,15 @@
 class ChargesController < ApplicationController
   def new
+    @amount = 500 # $5 in cents
+    @description = 'PURCHASE OF GOODS' # description of charge
   end
 
   def create
-    @amount = 500 # $5 in cents
+    amount = 500 # $5 in cents
     @customer = Stripe::Customer.create(email:   params[:stripeEmail],
                                        source:  params[:stripeToken])
     @charge = Stripe::Charge.create(customer:   @customer.id,
-                                   amount:      @amount,
+                                   amount:      amount,
                                    description: 'PURCHASE OF GOODS',
                                    currency:    'cad')
 
